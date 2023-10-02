cbd98a1e33568c60577048df426f7ae3e6181bc5
Marcel Ribeiro <mjrcel@hotmail.com>
2023-10-02 08:15:08 -0300

    criado uma api de controle para estacionamento

---
+++ parking-control/src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -0,0 +1,41 @@
+package com.api.parkingcontrol.services;
+
+import com.api.parkingcontrol.DTOs.ParkingSpotDto;
+import com.api.parkingcontrol.models.ParkingSpotModel;
+import com.api.parkingcontrol.repositories.ParkingSpotRepository;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+@Service
+public class ParkingSpotService {
+    final
+    ParkingSpotRepository parkingSpotRepository;
+
+    public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
+        this.parkingSpotRepository = parkingSpotRepository;
+    }
+
+@Transactional
+    public ParkingSpotModel save(ParkingSpotModel parkingSpotModel) {
+        return parkingSpotRepository.save(parkingSpotModel);
+    }
+
+    public boolean existsByLicensePlateCar(String licensePlateCar) {
+        return parkingSpotRepository.existsByLicensePlateCar(licensePlateCar);
+    }
+    public boolean existsByParkingSpotNumber(String parkingSpotNumber) {
+        return parkingSpotRepository.existsByParkingSpotNumber(parkingSpotNumber);
+    }
+    public boolean existsByApartmentAndBlock(String apartment, String block) {
+        return  parkingSpotRepository.existsByApartmentAndBlock(apartment, block);
+    }
+}
